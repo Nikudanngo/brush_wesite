fa4613a0ba39914f00b935c2de81b577c08fb380
nikudango <satoru_rutosa@icloud.com>
2022-01-02 20:29:30 +0900

    fix link

--- public/checkout.js
+++ public/checkout.js
@@ -42,7 +42,7 @@ async function handleSubmit(e) {
     elements,
     confirmParams: {
       // Make sure to change this to your payment completion page
-      return_url: "http://localhost:4242/checkout.html",
+      return_url: "public/checkout.js",
       receipt_email: document.getElementById("email").value,
     },
   });
