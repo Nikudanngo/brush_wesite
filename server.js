7503078acd2842ed069741f329a9204e08e2eabe
Nikudanngo <satoru_rutosa@icloud.com>
2022-01-05 20:43:18 +0900

    can get address

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-// This is your secret API key.
-const stripe = require('stripe')(SECRETAPIKEY);
-const { createSecretKey } = require('crypto');
+// This is your test secret API key.
+const stripe = require('stripe')('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');
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-const YOUR_DOMAIN = 'https://thoughtofoceans.com';
+const YOUR_DOMAIN = 'http://localhost:4242';
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
+    customer_email: 'customer@example.com',
+    submit_type: 'donate',
+    billing_address_collection: 'auto',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    },
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -19,6 +24,7 @@ app.post('/create-checkout-session', async (req, res) => {
     mode: 'payment',
     success_url: `${YOUR_DOMAIN}/success.html`,
     cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    automatic_tax: {enabled: true},
   });
 
   res.redirect(303, session.url);
