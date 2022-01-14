cabefd66bc61a5aeb5924d676254d1385455cb74
Nikudanngo <satoru_rutosa@icloud.com>
2022-01-14 13:50:35 +0900

    fix

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,12 @@ import { Link, NavLink } from 'react-router-dom';
 function Home () {
 //   const [tanukiti,setTanukiti]=React.useState(false);
     return (
-        <NavLink to="/Products">
-        <Button variant="contained" color="primary">
-            Invoices
-        </Button>
-        </NavLink>
+        // <NavLink to="/Products">
+        // <Button variant="contained" color="primary">
+        //     Invoices
+        // </Button>
+        // </NavLink>
+        
     );
 }
 export default Home;
