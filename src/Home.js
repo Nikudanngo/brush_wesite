a6e9a06ab58d379347b38bb204580d8bc9a23b4d
Nikudanngo <satoru_rutosa@icloud.com>
2022-01-19 15:55:16 +0900

    fix confrict

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,7 @@ import Slider from './Slider';
 function Home () {
 //   const [tanukiti,setTanukiti]=React.useState(false);
     return (
-<<<<<<< HEAD
-        // <NavLink to="/Products">
-        // <Button variant="contained" color="primary">
-        //     Invoices
-        // </Button>
-        // </NavLink>
-        
-=======
         <Slider />
->>>>>>> 385ea2e4846a7b
     );
 }
 export default Home;
