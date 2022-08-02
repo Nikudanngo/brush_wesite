eb7fe492f57518ad7cea3aea2d8082a9349e8b77
nikudango <satoru_rutosa@icloud.com>
2022-08-03 02:45:03 +0900

    fix slide css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    toolbar: {
-      ...theme.mixins.toolbar,
-    },
-  })
-);
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const classes = useStyles();
-  return (
-    // appbarのmargin分を追加
-    <div className={classes.toolbar}>
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
